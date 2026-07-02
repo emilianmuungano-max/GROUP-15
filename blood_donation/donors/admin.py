974475d7b1848749ddf00aa934d17866ab70ef1f
bobstuden <lucianlawrence06@gmail.com>
2026-07-02 20:36:43 +0300

    Update settings and admin config

--- blood_donation/donors/admin.py
+++ blood_donation/donors/admin.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Donor, Donation, BloodRequest
 
-admin.site.register(Donor)
-admin.site.register(Donation)
-admin.site.register(BloodRequest)
-
-# Register your models here.
-
-from django.contrib import admin
-from .models import Donor, Donation, BloodRequest
 
 admin.site.register(Donor)
 admin.site.register(Donation)
