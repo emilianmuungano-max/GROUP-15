2859cb2ae46342d33c50e4786f0ccc1e94c438d5
bobstuden <lucianlawrence06@gmail.com>
2026-07-02 20:15:28 +0300

    Clean project state

--- blood_donation/donors/admin.py
+++ blood_donation/donors/admin.py
@@ -7,7 +7,7 @@ admin.site.register(Donation)
 admin.site.register(BloodRequest)
 
 # Register your models here.
-=======
+
 from django.contrib import admin
 from .models import Donor, Donation, BloodRequest
 
@@ -16,4 +16,3 @@ admin.site.register(Donation)
 admin.site.register(BloodRequest)
 
 # Register your models here.
->>>>>>> 038084231e6d5fe0ffa70b357522cc1f39c569c8
